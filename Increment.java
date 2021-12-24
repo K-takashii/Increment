3d033abbe343ddd59c536cb75941800d8ec215ae
“K-takashii” <nanase.sigre.1627.yorushika@gmail.com>
2021-12-24 14:38:14 +0900

    Initial commit

---
+++ Increment.java
@@ -0,0 +1,12 @@
+class Increment {
+    public static void main(String[] args) {
+       int x = 8;
+       int y = 8;
+       
+       x++; //x=x+1と同じ意味(インクリメント)
+       y--; //y=y-1という意味(デクリメント)
+
+       System.out.println(x);
+       System.out.println(y);
+    }
+}
